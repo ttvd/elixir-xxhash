ec6216ccc0c26c6b2e78602314c43c1328d6ce76
Mykola Konyk <mykola@konyk.org>
2015-05-03 02:19:47 -0400

    lib: xxhash.ex: Work in progress.

--- lib/xxhash.ex
+++ lib/xxhash.ex
@@ -1,2 +1,52 @@
 defmodule XXHash do
+
+  use Bitwise
+
+  # Hashing constants.
+  @prime32_1 2654435761
+  @prime32_2 2246822519
+  @prime32_3 3266489917
+  @prime32_4 668265263
+  @prime32_5 374761393
+
+  @prime64_1 11400714785074694791
+  @prime64_2 14029467366897019727
+  @prime64_3 1609587929392839161
+  @prime64_4 9650029242287828579
+  @prime64_5 2870177450012600261
+
+  # Make sure integers are 32 or 64 bit long.
+  defmacrop mask_32(x), do: quote do: unquote(x) &&& 0xFFFFFFFF
+  defmacrop mask_64(x), do: quote do: unquote(x) &&& 0xFFFFFFFFFFFFFFFF
+
+  # Hash 32 bit integers.
+  @spec xxh32(binary | term, non_neg_integer) :: non_neg_integer
+  def xxh32(<<>>, length, seed), do: 0
+  def xxh32(input, length, seed) do
+  end
+
+  # Hash 64 bit integers.
+  @spec xxh64(binary | term, non_neg_integer) :: non_neg_integer
+  def xxh64(<<>>, length, seed), do: 0
+  def xxh64(input, length, seed) do
+  end
+
+  # Detect endianess.
+  @spec endianness() :: atom
+  defp endianness() when <<1::32-little>> == <<1::32-native>>, do: :endianness_little
+  defp endianness(), do: :endianness_big
+
+  # Swap bytes of 32 bit integer.
+  @spec byteswap32(integer) :: integer
+  defp byteswap32(value) when is_integer(value), do: <<y::32-big>>=<<value::32-little>>; y
+
+  # Swap bytes of 64 bit integer.
+  @spec byteswap64(integer) :: integer
+  defp byteswap64(value) when is_integer(value), do: <<y::64-big>>=<<value::64-little>>; y
+
+  # 
+
+  # Read 32 bit integer.
+  #defp read32(<<>>, :endianness_little), do:
+  # return endian==XXH_littleEndian ? *(U32*)ptr : XXH_swap32(*(U32*)ptr);
 end
