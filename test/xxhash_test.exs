f9af5fa0846d98d5230249180eb0f3bb3e158833
christianjgreen <christian.j.green@gmail.com>
2021-02-08 21:33:05 -0700

    add base test cases

--- test/xxhash_test.exs
+++ test/xxhash_test.exs
@@ -1,7 +1,25 @@
 defmodule XXHashTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "32 bit implementation" do
+    test "with empty string and default seed" do
+      assert XXHash.xxh32("") == 46_947_589
+    end
+
+    test "with a string < 16 characters" do
+      assert XXHash.xxh32("howdy") == 3_656_460_142
+    end
+
+    test "with a string < 16 characters using 8bit and 32bit buffers" do
+      assert XXHash.xxh32("howdyhowdy") == 2_955_902_467
+    end
+
+    test "with a string > 16 characters" do
+      assert XXHash.xxh32("howdyhowdymoomoohowdyhowdymoomoo") == 3_515_583_491
+    end
+
+    test "with a seed of '10'" do
+      assert XXHash.xxh32("howdyhowdymoomoohowdyhowdymoomoo", 10) == 1_124_996_114
+    end
   end
 end
