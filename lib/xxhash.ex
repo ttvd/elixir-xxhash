3a4ab67de8d0bd709afff1dad5ed8edbad709197
christianjgreen <christian.j.green@gmail.com>
2021-02-08 22:27:30 -0700

    extract primes to module attributes

--- lib/xxhash.ex
+++ lib/xxhash.ex
@@ -1,13 +1,18 @@
 defmodule XXHash do
+  @moduledoc """
+  Elixir implementation of XXHash.
+  Includes both 32 bit and 64 bit versions both outlined here:
+  https://github.com/Cyan4973/xxHash/blob/dev/doc/xxhash_spec.md
+  """
   use Bitwise
 
-  defmodule Int32 do
-    def prime_1, do: 2_654_435_761
-    def prime_2, do: 2_246_822_519
-    def prime_3, do: 3_266_489_917
-    def prime_4, do: 668_265_263
-    def prime_5, do: 374_761_393
+  @prime_32_1 2_654_435_761
+  @prime_32_2 2_246_822_519
+  @prime_32_3 3_266_489_917
+  @prime_32_4 668_265_263
+  @prime_32_5 374_761_393
 
+  defmodule Int32 do
     def add(a, b), do: (a + b) |> mask
     def sub(a, b), do: (a - b) |> mask
     def mul(a, b), do: (a * b) |> mask
@@ -26,13 +31,13 @@ defmodule XXHash do
     end
   end
 
-  defmodule Int64 do
-    def prime_1, do: 11_400_714_785_074_694_791
-    def prime_2, do: 14_029_467_366_897_019_727
-    def prime_3, do: 1_609_587_929_392_839_161
-    def prime_4, do: 9_650_029_242_287_828_579
-    def prime_5, do: 2_870_177_450_012_600_261
+  @prime_64_1 11_400_714_785_074_694_791
+  @prime_64_2 14_029_467_366_897_019_727
+  @prime_64_3 1_609_587_929_392_839_161
+  @prime_64_4 9_650_029_242_287_828_579
+  @prime_64_5 2_870_177_450_012_600_261
 
+  defmodule Int64 do
     def add(a, b), do: (a + b) |> mask
     def sub(a, b), do: (a - b) |> mask
     def mul(a, b), do: (a * b) |> mask
@@ -57,6 +62,7 @@ defmodule XXHash do
   @spec xxh32(binary | term, non_neg_integer) :: non_neg_integer
   def xxh32(input, seed), do: xxh32(input, String.length(input), seed)
 
+  # 32 bit empty binary hardcoded hash
   @spec xxh32(binary | term, non_neg_integer, non_neg_integer) :: non_neg_integer
   def xxh32(<<>>, _length, _seed), do: 46_947_589
 
@@ -66,50 +72,54 @@ defmodule XXHash do
       if length >= 16 do
         do_xxh32(0, seed, input)
       else
-        {Int32.add(seed, Int32.prime_5()), input}
+        {Int32.add(seed, @prime_32_5), input}
       end
 
     h32
     |> Int32.add(length)
     |> do_xxh32(seed, buffer)
     |> Int32.rshift_xor(15)
-    |> Int32.mul(Int32.prime_2())
+    |> Int32.mul(@prime_32_2)
     |> Int32.rshift_xor(13)
-    |> Int32.mul(Int32.prime_3())
+    |> Int32.mul(@prime_32_3)
     |> Int32.rshift_xor(16)
   end
 
+  # Seed accumulators
   @spec do_xxh32(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
   defp do_xxh32(h, seed, <<_a::32, _b::32, _c::32, _d::32, _rest::binary>> = all) do
-    v1 = Int32.add(seed, Int32.prime_1()) |> Int32.add(Int32.prime_2())
-    v2 = Int32.add(seed, Int32.prime_2())
+    v1 = Int32.add(seed, @prime_32_1) |> Int32.add(@prime_32_2)
+    v2 = Int32.add(seed, @prime_32_2)
     v3 = Int32.add(seed, 0)
-    v4 = Int32.sub(seed, Int32.prime_1())
+    v4 = Int32.sub(seed, @prime_32_1)
     do_xxh32(h, seed, all, {v1, v2, v3, v4})
   end
 
   @spec do_xxh32(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
   defp do_xxh32(h, _seed, <<>>), do: h
 
+  # Consume remaining input in 32 bit chunks
   @spec do_xxh32(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
   defp do_xxh32(h, seed, <<p::32, rest::binary>>) do
     Int32.read(<<p::32>>)
-    |> Int32.mul(Int32.prime_3())
+    |> Int32.mul(@prime_32_3)
     |> Int32.add(h)
     |> Int32.rotl(17)
-    |> Int32.mul(Int32.prime_4())
+    |> Int32.mul(@prime_32_4)
     |> do_xxh32(seed, rest)
   end
 
+  # Consume remaining input in 8 bit chunks
   @spec do_xxh32(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
   defp do_xxh32(h, seed, <<p::8, rest::binary>>) do
-    Int32.mul(p, Int32.prime_5())
+    Int32.mul(p, @prime_32_5)
     |> Int32.add(h)
     |> Int32.rotl(11)
-    |> Int32.mul(Int32.prime_1())
+    |> Int32.mul(@prime_32_1)
     |> do_xxh32(seed, rest)
   end
 
+  # Process stripes
   @spec do_xxh32(non_neg_integer, non_neg_integer, binary | term, tuple) :: non_neg_integer
   defp do_xxh32(h, seed, <<a::32, b::32, c::32, d::32, rest::binary>>, {v1, v2, v3, v4}) do
     do_xxh32(
@@ -121,6 +131,7 @@ defmodule XXHash do
     )
   end
 
+  # Convergence
   @spec do_xxh32(non_neg_integer, non_neg_integer, binary | term, tuple) :: non_neg_integer
   defp do_xxh32(_h, _seed, rest, {v1, v2, v3, v4}) do
     {Int32.rotl(v1, 1) + Int32.rotl(v2, 7) + Int32.rotl(v3, 12) + Int32.rotl(v4, 18), rest}
@@ -129,10 +140,10 @@ defmodule XXHash do
   defp round32(acc_n, lane_n) do
     lane_n
     |> Int32.read()
-    |> Int32.mul(Int32.prime_2())
+    |> Int32.mul(@prime_32_2)
     |> Int32.add(acc_n)
     |> Int32.rotl(13)
-    |> Int32.mul(Int32.prime_1())
+    |> Int32.mul(@prime_32_1)
   end
 
   ## 64 bit implementation
@@ -143,6 +154,7 @@ defmodule XXHash do
   @spec xxh64(binary | term, non_neg_integer) :: non_neg_integer
   def xxh64(input, seed), do: xxh64(input, String.length(input), seed)
 
+  # 64 bit empty binary hardcoded hash
   @spec xxh64(binary | term, non_neg_integer, non_neg_integer) :: non_neg_integer
   def xxh64(<<>>, _length, _seed), do: 17_241_709_254_077_376_921
 
@@ -152,59 +164,64 @@ defmodule XXHash do
       if length >= 32 do
         do_xxh64(0, seed, input)
       else
-        {Int64.add(seed, Int64.prime_5()), input}
+        {Int64.add(seed, @prime_64_5), input}
       end
 
     h64
     |> Int64.add(length)
     |> do_xxh64(seed, buffer)
     |> Int64.rshift_xor(33)
-    |> Int64.mul(Int64.prime_2())
+    |> Int64.mul(@prime_64_2)
     |> Int64.rshift_xor(29)
-    |> Int64.mul(Int64.prime_3())
+    |> Int64.mul(@prime_64_3)
     |> Int64.rshift_xor(32)
   end
 
+  # Seed accumulators
   @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
   defp do_xxh64(h, seed, <<_a::64, _b::64, _c::64, _d::64, _rest::binary>> = all) do
-    v1 = Int64.add(seed, Int64.prime_1()) |> Int64.add(Int64.prime_2())
-    v2 = Int64.add(seed, Int64.prime_2())
+    v1 = Int64.add(seed, @prime_64_1) |> Int64.add(@prime_64_2)
+    v2 = Int64.add(seed, @prime_64_2)
     v3 = Int64.add(seed, 0)
-    v4 = Int64.sub(seed, Int64.prime_1())
+    v4 = Int64.sub(seed, @prime_64_1)
     do_xxh64(h, seed, all, {v1, v2, v3, v4})
   end
 
   @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
   defp do_xxh64(h, _seed, <<>>), do: h
 
+  # Consume remaining input in 64 bit chunks
   @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
   defp do_xxh64(h, seed, <<p::64, rest::binary>>) do
     round64(0, Int64.read(<<p::64>>))
     |> Int64.xor(h)
     |> Int64.rotl(27)
-    |> Int64.mul(Int64.prime_1())
-    |> Int64.add(Int64.prime_4())
+    |> Int64.mul(@prime_64_1)
+    |> Int64.add(@prime_64_4)
     |> do_xxh64(seed, rest)
   end
+
+  # Consume remaining input in 32 bit chunks
 
   @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
   defp do_xxh64(h, seed, <<p::32, rest::binary>>) do
     Int32.read(<<p::32>>)
-    |> Int64.mul(Int64.prime_1())
+    |> Int64.mul(@prime_64_1)
     |> Int64.xor(h)
     |> Int64.rotl(23)
-    |> Int64.mul(Int64.prime_2())
-    |> Int64.add(Int64.prime_3())
+    |> Int64.mul(@prime_64_2)
+    |> Int64.add(@prime_64_3)
     |> do_xxh64(seed, rest)
   end
 
+  # Consume remaining input in 8 bit chunks
   @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
   defp do_xxh64(h, seed, <<p::8, rest::binary>>) do
     p
-    |> Int64.mul(Int64.prime_5())
+    |> Int64.mul(@prime_64_5)
     |> Int64.xor(h)
     |> Int64.rotl(11)
-    |> Int64.mul(Int64.prime_1())
+    |> Int64.mul(@prime_64_1)
     |> do_xxh64(seed, rest)
   end
 
@@ -233,17 +250,17 @@ defmodule XXHash do
 
   defp round64(acc_n, lane_n) do
     lane_n
-    |> Int64.mul(Int64.prime_2())
+    |> Int64.mul(@prime_64_2)
     |> Int64.add(acc_n)
     |> Int64.rotl(31)
-    |> Int64.mul(Int64.prime_1())
+    |> Int64.mul(@prime_64_1)
   end
 
   defp merge64(acc, acc_n) do
     0
     |> round64(acc_n)
     |> Int64.xor(acc)
-    |> Int64.mul(Int64.prime_1())
-    |> Int64.add(Int64.prime_4())
+    |> Int64.mul(@prime_64_1)
+    |> Int64.add(@prime_64_4)
   end
 end
