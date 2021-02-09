498c575b41867f2d5dde18d41a41c54e52a215b3
christianjgreen <christian.j.green@gmail.com>
2021-02-08 22:02:19 -0700

    draft 64 bit implementation

--- lib/xxhash.ex
+++ lib/xxhash.ex
@@ -23,7 +23,7 @@ defmodule XXHash do
     defp byteswap(a) do
       <<b::32-big>> = <<a::32-little>>
       b
-  end
+    end
   end
 
   defmodule Int64 do
@@ -48,7 +48,7 @@ defmodule XXHash do
     defp byteswap(a) do
       <<b::64-big>> = <<a::64-little>>
       b
-  end
+    end
   end
 
   @spec xxh32(binary | term, non_neg_integer, non_neg_integer) :: non_neg_integer
@@ -133,5 +133,117 @@ defmodule XXHash do
     |> Int32.add(acc_n)
     |> Int32.rotl(13)
     |> Int32.mul(Int32.prime_1())
+  end
+
+  ## 64 bit implementation
+
+  @spec xxh64(binary | term, non_neg_integer, non_neg_integer) :: non_neg_integer
+  def xxh64(input), do: xxh64(input, String.length(input), 0)
+
+  @spec xxh64(binary | term, non_neg_integer) :: non_neg_integer
+  def xxh64(input, seed), do: xxh64(input, String.length(input), seed)
+
+  @spec xxh64(binary | term, non_neg_integer, non_neg_integer) :: non_neg_integer
+  def xxh64(<<>>, _length, _seed), do: 17_241_709_254_077_376_921
+
+  @spec xxh64(binary | term, non_neg_integer, non_neg_integer) :: non_neg_integer
+  def xxh64(input, length, seed) do
+    {h64, buffer} =
+      if length >= 32 do
+        do_xxh64(0, seed, input)
+      else
+        {Int64.add(seed, Int64.prime_5()), input}
+      end
+
+    h64
+    |> Int64.add(length)
+    |> do_xxh64(seed, buffer)
+    |> Int64.rshift_xor(33)
+    |> Int64.mul(Int64.prime_2())
+    |> Int64.rshift_xor(29)
+    |> Int64.mul(Int64.prime_3())
+    |> Int64.rshift_xor(32)
+  end
+
+  @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
+  defp do_xxh64(h, seed, <<_a::64, _b::64, _c::64, _d::64, _rest::binary>> = all) do
+    v1 = Int64.add(seed, Int64.prime_1()) |> Int64.add(Int64.prime_2())
+    v2 = Int64.add(seed, Int64.prime_2())
+    v3 = Int64.add(seed, 0)
+    v4 = Int64.sub(seed, Int64.prime_1())
+    do_xxh64(h, seed, all, {v1, v2, v3, v4})
+  end
+
+  @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
+  defp do_xxh64(h, _seed, <<>>), do: h
+
+  @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
+  defp do_xxh64(h, seed, <<p::64, rest::binary>>) do
+    round64(0, Int64.read(<<p::64>>))
+    |> Int64.xor(h)
+    |> Int64.rotl(27)
+    |> Int64.mul(Int64.prime_1())
+    |> Int64.add(Int64.prime_4())
+    |> do_xxh64(seed, rest)
+  end
+
+  @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
+  defp do_xxh64(h, seed, <<p::32, rest::binary>>) do
+    Int32.read(<<p::32>>)
+    |> Int64.mul(Int64.prime_1())
+    |> Int64.xor(h)
+    |> Int64.rotl(23)
+    |> Int64.mul(Int64.prime_2())
+    |> Int64.add(Int64.prime_3())
+    |> do_xxh64(seed, rest)
+  end
+
+  @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term) :: non_neg_integer
+  defp do_xxh64(h, seed, <<p::8, rest::binary>>) do
+    p
+    |> Int64.mul(Int64.prime_5())
+    |> Int64.xor(h)
+    |> Int64.rotl(11)
+    |> Int64.mul(Int64.prime_1())
+    |> do_xxh64(seed, rest)
+  end
+
+  @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term, tuple) :: non_neg_integer
+  defp do_xxh64(h, seed, <<a::64, b::64, c::64, d::64, rest::binary>>, {v1, v2, v3, v4}) do
+    do_xxh64(
+      h,
+      seed,
+      rest,
+      {round64(v1, Int64.read(<<a::64>>)), round64(v2, Int64.read(<<b::64>>)),
+       round64(v3, Int64.read(<<c::64>>)), round64(v4, Int64.read(<<d::64>>))}
+    )
+  end
+
+  @spec do_xxh64(non_neg_integer, non_neg_integer, binary | term, tuple) :: non_neg_integer
+  defp do_xxh64(_h, _seed, rest, {v1, v2, v3, v4}) do
+    acc =
+      (Int64.rotl(v1, 1) + Int64.rotl(v2, 7) + Int64.rotl(v3, 12) + Int64.rotl(v4, 18))
+      |> merge64(v1)
+      |> merge64(v2)
+      |> merge64(v3)
+      |> merge64(v4)
+
+    {acc, rest}
+  end
+
+  defp round64(acc_n, lane_n) do
+    lane_n
+    |> Int64.mul(Int64.prime_2())
+    |> Int64.add(acc_n)
+    |> Int64.rotl(31)
+    |> Int64.mul(Int64.prime_1())
+  end
+
+  defp merge64(acc, acc_n) do
+    0
+    |> round64(acc_n)
+    |> Int64.xor(acc)
+    |> Int64.mul(Int64.prime_1())
+    |> Int64.add(Int64.prime_4())
   end
 end
